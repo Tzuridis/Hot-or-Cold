abfcbd8f09fc9d5ca2259a9579efe564c2a45c14
Ted Tzuridis <tzuridis@gmail.com>
2016-07-11 20:13:37 -0400

    Almost

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-$(document).ready(function() {
 
+$(document).ready(function() {
+    Random();
+    console.log(Choose)
     /*--- Display information modal box ---*/
     $(".what").click(function() {
         $(".overlay").fadeIn(1000);
@@ -16,19 +18,25 @@ $(document).ready(function() {
         $('#count').html(0);
         $('.text').empty();
         $('.guessBox').empty();
+        Random();
+        console.log(Choose)
     });
 
     $('form').submit(function() {
+        25
         event.preventDefault();
         var guess = $('.text').val();
         $('.guessBox').append('<li>' + guess + '</li>');
         $('.text').val('');
         if (isNaN(guess)) {
             alert('Unable to comply, please enter a valid number')
-     		$('.guessBox').empty();       
+            $('.guessBox').empty();
+            $('#count').empty();
+            $('#count').html(-1);
         }
-       var $counter = $('#count');
-       $counter.html((parseInt($counter.html(), 10) || 0) + 1);
+        Guessing(guess);
+        var $counter = $('#count');
+        $counter.html((parseInt($counter.html(), 10) || 0) + 1);
 
     })
 
@@ -37,6 +45,15 @@ $(document).ready(function() {
 
 // Game Mechanics
 
-function Game(guess) {
-
+function Random() {
+    Choose = Math.floor(Math.random()*100)+1;
 }
+
+
+function Guessing(guess) {
+var guess =  $(".guessBox:last-child")
+    if (guess === Choose) {
+       $("h2#feedback").html("You win!")
+    } 
+}
+
